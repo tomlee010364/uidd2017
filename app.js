8669b5f80aa98f5b05d72c6cb7c68a11cd7100d9
tomlee010364 <tomlee010364@gmail.com>
2017-03-26 16:18:46 +0800

    i

---
+++ app.js
@@ -0,0 +1,24 @@
+$(function(){
+  $('#toap').click(function(){
+    $('html,body').animate({scrollTop:$('#ap').offset().top},800);
+    return false;
+  });
+});
+$(function(){
+  $('#tosp').click(function(){
+    $('html,body').animate({scrollTop:$('#sp').offset().top},800);
+    return false;
+  });
+});
+$(function(){
+  $('#tonp').click(function(){
+    $('html,body').animate({scrollTop:$('#np').offset().top},800);
+    return false;
+  });
+});
+$(function(){
+  $('#tocp').click(function(){
+    $('html,body').animate({scrollTop:$('#cp').offset().top},800);
+    return false;
+  });
+});
